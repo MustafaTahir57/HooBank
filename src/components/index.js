ef6618fe5de5338d91586d1b95d4d4d35527cb5e
Mustafa Tahir <mustafatahir802@gmail.com>
2023-08-01 17:15:34 +0500

    A new responsive site

---
+++ src/components/index.js
@@ -0,0 +1,45 @@
+import Navbar from './Navbar'
+import Footer from './Footer'
+import Hero from './Hero'
+import Billing from './Billing'
+import Business from './Business'
+import CardDeal from './CardDeal'
+import Clients from './Clients'
+import Button from './Button'
+import CTA from './CTA'
+import Stats from './Stats'
+import Testimonials from './Testimonials'
+import FeedbackCard from './FeedbackCard'
+import GetStarted from './GetStarted'
+
+export{
+    
+    Navbar,
+    Footer,
+    Hero,
+    Billing,
+    Business,
+    CardDeal,
+    Clients,
+    Button,
+    CTA,
+    Stats,
+    Testimonials,
+    FeedbackCard,
+    GetStarted
+
+}
+
+// import Navbar from './components/Navbar'
+// import Footer from './components/Footer'
+// import Hero from './components/Hero'
+// import Billing from './components/Billing'
+// import Business from './components/Business'
+// import CardDeal from './components/CardDeal'
+// import Clients from './components/Clients'
+// import Button from './components/Button'
+// import CTA from './components/CTA'
+// import Stats from './components/Stats'
+// import Testimonials from './components/Testimonials'
+// import FeedbackCard from './components/FeedbackCard'
+// import GetStarted from './components/GetStarted'
